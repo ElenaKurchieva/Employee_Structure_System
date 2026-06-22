f3f2548167db429e6468d69ca3b3a4834b287971
Elena Kyurchieva <elena.kyurchieva@smartit.bg>
2026-06-22 22:50:43 +0300

    Refactor application styles and update assembly information
    - Updated assembly informational version for Application, Domain, and Infrastructure projects.
    - Modified layout links in _Layout.cshtml to remove unnecessary text-dark class.
    - Enhanced navigation link styles in site.css for better user experience, including hover and active states.
    - Updated sourcelink JSON files to reflect new commit hashes.
    - Rebuilt binaries for Application, Domain, and Infrastructure projects.

--- EmployeeStructureSystem.Application/obj/Debug/net9.0/EmployeeStructureSystem.Application.AssemblyInfo.cs
+++ EmployeeStructureSystem.Application/obj/Debug/net9.0/EmployeeStructureSystem.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeStructureSystem.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8474961a69ae8fe1074011f43cabb07443a936ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95fe8d4818773a8cf205cb55ce51a9552dd4b6c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeStructureSystem.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeStructureSystem.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
